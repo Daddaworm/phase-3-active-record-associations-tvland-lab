d47aea1661b66716df7e240e70792fe4a517bad3
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-17 17:43:21 -0400

    commit tree 488765846088752342cacc376cfb3199bfe44d3c

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+    belongs_to :network 
+    has_many :characters
+    has_many :actors, through: :characters
+
+
+    def actors_list
+        # returns a list of the full names of each actor associated with the show 
+        actors.map do |actor|
+            "#{actor.first_name} #{actor.last_name}"
+        # binding.pry
+        end
+
+    end
+
 end
